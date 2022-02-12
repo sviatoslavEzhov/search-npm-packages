da7602db79946c2537bd3a7a3c2577c8119b77a8
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 21:35:28 +0200

    refactoring(component): PackageItem; add move components from vuetify

--- src/components/PackageItem.vue
+++ src/components/PackageItem.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="package-item">
-    <h3>{{ packageItem.name }}</h3>
-    <div>
-			<img :src="packageItem.owner.avatar" alt="" :style="{width: '20px', height: '20px' }">
+  <v-card class="package-item">
+    <v-card-title>{{ packageItem.name }}</v-card-title>
+		<v-divider></v-divider>
+
+    <v-card-text class="d-flex">
+			<img
+				class="mr-2"
+				:src="packageItem.owner.avatar"
+				:alt="packageItem.owner.name"
+				:style="{width: '20px', height: '20px' }"
+			>
 			<span>{{packageItem.owner.name}}</span>
-			<a :href="packageItem.owner.link">Link</a>
-		</div>
-    <p v-html="packageItem.description"></p>
+		</v-card-text>
 
-		<v-btn
-      color="primary"
-      dark
-      @click="$emit('dialog', packageItem)"
-    >More info</v-btn>
+    <v-card-text v-html="packageItem.description"></v-card-text>
 
-
-  </div>
+		<v-card-actions>
+			<v-btn
+				color="primary"
+				dark
+				@click="$emit('dialog', packageItem)"
+			>More info</v-btn>
+		</v-card-actions>
+  </v-card>
 </template>
 
 <script>
