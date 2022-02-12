320d25ff7c83b6af255d3597dd1baa7c7d8d1d3c
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 13:06:48 +0200

    add(js): api; add raw api

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import algoliasearch from 'algoliasearch';
+
+const APP_ID = 'OFCNCOG2CU';
+const API_KEY = 'f54e21fa3a2a0160595bb058179bfb1e';
+const INDEX = 'npm-search';
+
+const client = algoliasearch(APP_ID, API_KEY);
+const index = client.initIndex(INDEX);
+
+const defaultOptions = {
+  page: 0,
+  hitsPerPage: 10
+}
+
+// TODO add URL params
+const fetching = async (params) => {
+  const query = params?.query ?? '';
+  const requestOptions = {...defaultOptions, ...params?.requestOptions };
+
+  return index.search(query, requestOptions).then((data) => data);
+}
+
+export { fetching }
