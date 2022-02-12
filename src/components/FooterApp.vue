c47ec092f88b4ee2e95328d3ae952141fb9549c9
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 21:32:59 +0200

    add(component): FooterApp; add FooterApp component

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="py-4 d-flex">
+		<v-img
+			class="author-photo mr-4"
+			lazy-src="@/assets/EzhovSvyatoslav.jpeg"
+			aspect-ratio="1"
+			width="80"
+			src="@/assets/EzhovSvyatoslav.jpeg"
+		></v-img>
+
+		<div class="author-info">
+			<div class="text-h6">Ezhov Svyatoslav</div>
+			<a href="https://github.com/sviatoslavEzhov" target="_blank" rel="noopener noreferrer nofollow">
+				<div class="text">Github</div>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+	.author-photo { border-radius: 50%; }
+</style>
