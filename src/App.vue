d21897df5184172330d7bfd56ca2149d9913b531
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 21:38:20 +0200

    update(component): App; update template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-	<router-view/>
+	<v-app>
+		<!-- header -->
+		<v-app-bar dark app>
+			<header-app />
+		</v-app-bar>
+
+		<!-- content-->
+		<v-main app>
+			<router-view/>
+		</v-main>
+
+		<!-- footer -->
+		<v-footer dark app>
+			<footer-app />
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-export default {
+import HeaderApp from '@/components/HeaderApp.vue';
+import FooterApp from '@/components/FooterApp.vue';
 
+export default {
+	components: {
+		HeaderApp,
+		FooterApp
+	}
 }
 </script>
 
