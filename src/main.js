a3644cd255cf9ef13ef3330fba1fdc6b0151cd25
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 13:48:26 +0200

    add(plugin): vuetifyjs; add vuetifyjs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import InstantSearch from 'vue-instantsearch'
+import vuetify from './plugins/vuetify'
 
 Vue.use(InstantSearch)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
