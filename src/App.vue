c1cb796f4ad0b2a8c6e558d35f1d1f3efebd99e5
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2022-02-12 13:07:21 +0200

    update(component): App; add raw search input & search result

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
-  Demo
+  <div>
+    <input type="text" v-model="query">
+    <div
+      class="package-item"
+      v-for="(item, index) in packages"
+      :key="item.objectID"
+    >{{index + 1}}. {{item.name}}</div>
+  </div>
 </template>
 
 <script>
+import { fetching } from './api';
 
 export default {
-  name: 'App',
-}
+	name: "App",
+
+	data() {
+		return {
+			packages: [],
+			isLoading: true,
+			page: 0,
+			pages: null,
+			query: ''
+		}
+	},
+
+	methods: {
+		setNewData() {
+			fetching({
+				query: this.query,
+				requestOptions: {
+					page: this.page,
+				}
+			}).then((data) => {
+				console.log('data', data.hits)
+				this.packages = data.hits
+			})
+		}
+	},
+
+	watch: {
+		page: function() { this.setNewData() },
+		query: function() { this.setNewData() },
+	},
+
+	mounted() {
+		this.setNewData()
+	},
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
